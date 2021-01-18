6e2f6b603de9b650ec848b3f0c40582e1b432062
cherenkor <buhanka1994@gmail.com>
2021-01-19 00:14:45 +0200

    Add config module

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/migrations/*.js'],
     cli: {
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './modules/coffees/coffees.module';
 import { CoffeeRatingModule } from './modules/coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'pass123',
-            database: 'postgres',
+            database: process.env.DATABASE_NAME,
+            host: process.env.DATABASE_HOST,
+            port: +process.env.DATABASE_PORT,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
             autoLoadEntities: true,
             synchronize: process.env.NODE_ENV === 'production' ? false : true, // Note: Don't use on Prod
         }),
