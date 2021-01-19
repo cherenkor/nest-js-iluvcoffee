9a9886c7ebc8da145a573fd2e1a008c386e5adb7
cherenkor <buhanka1994@gmail.com>
2021-01-19 23:17:52 +0200

    Add swagger module & setup it

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -23,6 +24,15 @@ async function bootstrap() {
         new TimeoutInterceptor(),
     );
     app.useGlobalFilters(new HttpExceptionFilter());
+
+    const options = new DocumentBuilder()
+        .setTitle('ILuvCoffee')
+        .setDescription('Coffee Application written on Nest.js')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
