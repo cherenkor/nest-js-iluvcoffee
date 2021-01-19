71ef7cad40e4aff5441bbe70c4d6d7135bd5012d
cherenkor <buhanka1994@gmail.com>
2021-01-19 22:50:40 +0200

    Create WrapResponse & Timeout interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
@@ -15,6 +17,10 @@ async function bootstrap() {
                 enableImplicitConversion: true,
             },
         }),
+    );
+    app.useGlobalInterceptors(
+        new WrapResponseInterceptor(),
+        new TimeoutInterceptor(),
     );
     app.useGlobalFilters(new HttpExceptionFilter());
     await app.listen(3000);
