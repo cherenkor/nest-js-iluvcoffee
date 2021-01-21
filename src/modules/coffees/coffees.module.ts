6e589a000d01bb92671fab4e0d0775004a719d88
cherenkor <buhanka1994@gmail.com>
2021-01-22 00:11:05 +0200

    Add mongodb

--- src/modules/coffees/coffees.module.ts
+++ src/modules/coffees/coffees.module.ts
@@ -1,6 +1,19 @@
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesController } from './controllers/coffees.controller';
 import { CoffeesService } from './services/coffees.service';
 import { Module } from '@nestjs/common';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Coffee.name,
+                schema: CoffeeSchema,
+            },
+        ]),
+    ],
+    controllers: [CoffeesController],
+    providers: [CoffeesService],
+})
 export class CoffeesModule {}
